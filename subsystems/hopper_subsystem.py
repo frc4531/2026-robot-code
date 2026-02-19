2dc652784bde3f14d46357c71789e2be8ae22a3b
Sam T <corgidadog@gmail.com>
2026-02-18 19:07:26 -0600

    hopper sub changes

---
+++ subsystems/hopper_subsystem.py
@@ -0,0 +1,20 @@
+import ntcore
+import rev
+import wpilib
+from commands2 import SubsystemBase
+
+
+class HopperSubsystem(SubsystemBase):
+    # Create a new LiftSubsystem
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.vectored_bar_motor = rev.SparkFlex(3, rev.SparkMax.MotorType.kBrushless)
+        self.feeder_motor = rev.SparkFlex(4, rev.SparkMax.MotorType.kBrushless)
+
+        self.bottom_intake_motor.setInverted(True)
+
+    def set_hopper_speed(self, speed):
+        self.vectored_bar_motor.set(speed)
+        self.feeder_motor.set(speed)
